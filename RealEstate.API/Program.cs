4080a5f228eac3f95c5d25606c3248c3246d2e75
Md Rizwan Ahmad <mdrizwanahmad078@gmail.com>
2026-08-19 09:32:07 +0530

    swagger update

--- RealEstate.API/Program.cs
+++ RealEstate.API/Program.cs
@@ -65,11 +65,8 @@ builder.Services.AddScoped<IS3Service, RealEstate.Infrastructure.Services.S3Serv
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("AllowFrontend");
 
